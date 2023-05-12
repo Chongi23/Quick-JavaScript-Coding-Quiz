02a92cfb696b52edab97e57aecfeb75b9c717175
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-05-11 22:42:00 -0500

    any updates left

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
 let start = document.querySelector('#start');
+start.addEventListener("click", function(){
+    document.querySelector("#question").textContent="What happens when user clicks HTML element?"
+    document.querySelector("#A").textContent="onplap"
+    document.querySelector("#B").textContent="onlick"
+    document.querySelector("#C").textContent="onchange"
+    document.querySelector("#D").textContent="pancakes"
+})
+
+let next = document.querySelector('#next');
+next.assEventListener("click", function(){
+
+
+ 
+ 
+})
